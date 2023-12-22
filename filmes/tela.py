da91f32eb39f102699ff03c17c5dd8b7d91241fb
brunobbm97 <brunobbmachado97@outlook.com>
2023-12-21 21:07:34 -0300

    final_post

--- filmes/tela.py
+++ filmes/tela.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 
+from io import BytesIO
+
 from datetime import datetime
 
 #importanto a funcao principal
@@ -88,7 +90,7 @@ def resultado(i):
     frame_1.grid(row=0, column=0, sticky=NSEW, pady=5)
 
     # nome
-    nome = Label(frame_1, text=f'{titles[0]}',height=2, padx=10,pady=5, wraplength=100, justify='left',
+    nome = Label(frame_1, text=f'{titles[0]}',width=17, height=2, padx=10,pady=5, wraplength=100, justify='left',
                  relief=SOLID, anchor=NW, font=('Ivy 9 bold'), bg=co1, fg=co0, bd=1, highlightbackground='white')
     nome.place(x=7, y=260)
 
@@ -102,11 +104,107 @@ def resultado(i):
     l_data_1.place(x=5, y=310)
 
     # Voto
-    l_voto_1 = Label(frame_1, text=f'Media de votos: {votos[0]/10}',
+    l_voto_1 = Label(frame_1, text=f'Media de votos: {votos[0]}/10',
                      anchor=NW, font=('Ivy 8'), bg=co1, fg=co0)
     l_voto_1.place(x=5, y=330)
 
+    # Obtendo a imagem
 
+    # Obter o url da imagem a partir da api
+    response_1 = requests.get(f'{poster[0]}')
+    image_url_1 = response_1.url
+
+    # Carregando a imagem
+    response_1 = requests.get(image_url_1)
+    img_1 = Image.open(BytesIO(response_1.content))
+
+    capa_1 = img_1
+    capa_1 = capa_1.resize((150,250))
+    capa_1 = ImageTk.PhotoImage(capa_1)
+
+    l_capa_1 = Label(frame_1, image=capa_1, padx=5, bg=co1, fg=co0)
+    l_capa_1.place(x=5, y=0)
+
+    # filme 2
+    frame_2 = Frame(frameBaixo, width=150, height=400, bg=co1)
+    frame_2.grid(row=0, column=1, sticky=NSEW, pady=5)
+
+    # nome
+    nome = Label(frame_2, text=f'{titles[1]}', width=17, height=2, padx=10, pady=5, wraplength=100, justify='left',
+                 relief=SOLID, anchor=NW, font=('Ivy 9 bold'), bg=co1, fg=co0, bd=1, highlightbackground='white')
+    nome.place(x=7, y=260)
+
+    # Data
+    data_string_2 = f'{data[1]}'
+    data_2 = datetime.strptime(data_string_2, '%Y-%m-%d')
+    data_formatada = data_2.strftime('%B %Y')
+
+    l_data_2 = Label(frame_2, text=f'Lançamento: {data_formatada}',
+                     anchor=NW, font=('Ivy 8'), bg=co1, fg=co0)
+    l_data_2.place(x=5, y=310)
+
+    # Voto
+    l_voto_2 = Label(frame_2, text=f'Media de votos: {votos[1]}/10',
+                     anchor=NW, font=('Ivy 8'), bg=co1, fg=co0)
+    l_voto_2.place(x=5, y=330)
+
+    # Obtendo a imagem
+
+    # Obter o url da imagem a partir da api
+    response_2 = requests.get(f'{poster[1]}')
+    image_url_2 = response_2.url
+
+    # Carregando a imagem
+    response_2 = requests.get(image_url_2)
+    img_2 = Image.open(BytesIO(response_2.content))
+
+    capa_2 = img_2
+    capa_2 = capa_2.resize((150, 250))
+    capa_2 = ImageTk.PhotoImage(capa_2)
+
+    l_capa_2 = Label(frame_2, image=capa_2, padx=5, bg=co1, fg=co0)
+    l_capa_2.place(x=5, y=0)
+
+
+    # filme 3
+    frame_3 = Frame(frameBaixo, width=150, height=400, bg=co1)
+    frame_3.grid(row=0, column=2, sticky=NSEW, pady=5)
+
+    # nome
+    nome = Label(frame_3, text=f'{titles[2]}', width=17, height=2, padx=10, pady=5, wraplength=100, justify='left',
+                 relief=SOLID, anchor=NW, font=('Ivy 9 bold'), bg=co1, fg=co0, bd=1, highlightbackground='white')
+    nome.place(x=7, y=260)
+
+    # Data
+    data_string_3 = f'{data[2]}'
+    data_3 = datetime.strptime(data_string_3, '%Y-%m-%d')
+    data_formatada = data_3.strftime('%B %Y')
+
+    l_data_3 = Label(frame_3, text=f'Lançamento: {data_formatada}',
+                     anchor=NW, font=('Ivy 8'), bg=co1, fg=co0)
+    l_data_3.place(x=5, y=310)
+
+    # Voto
+    l_voto_3 = Label(frame_3, text=f'Media de votos: {votos[2]}/10',
+                     anchor=NW, font=('Ivy 8'), bg=co1, fg=co0)
+    l_voto_3.place(x=5, y=330)
+
+    # Obtendo a imagem
+
+    # Obter o url da imagem a partir da api
+    response_3 = requests.get(f'{poster[2]}')
+    image_url_3 = response_3.url
+
+    # Carregando a imagem
+    response_3 = requests.get(image_url_3)
+    img_3 = Image.open(BytesIO(response_3.content))
+
+    capa_3 = img_3
+    capa_3 = capa_3.resize((150, 250))
+    capa_3 = ImageTk.PhotoImage(capa_3)
+
+    l_capa_3 = Label(frame_3, image=capa_3, padx=5, bg=co1, fg=co0)
+    l_capa_3.place(x=5, y=0)
 
 
 
